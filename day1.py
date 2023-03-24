99cadfa92234e63508dcf052e2ac6afb4e2feb38
Simon Magus <simonmagusgno@gmail.com>
2023-03-24 12:43:14 +0200

    Edit and update da1.py

--- day1.py
+++ day1.py
@@ -1,9 +1,15 @@
 #Ask user for input and display todos
 
-user_prompt = "Enter a todo: "
+user_prompt = "Enter a todo:"
 todo1 = input(user_prompt)
 todo2 = input(user_prompt)
 todo3 = input(user_prompt)
 
 todos = [todo1, todo2, todo3]
 print(todos)
+
+
+title = "Enter the title: "
+user_title = input(title)
+
+print("The title length is " + str(len(user_title)))
